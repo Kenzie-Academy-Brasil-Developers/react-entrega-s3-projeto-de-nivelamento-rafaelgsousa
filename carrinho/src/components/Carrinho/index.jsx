62bf1416444b9150a493f6bf9678b09e23dc9ab2
rafaeldev <elderrafaelgomes@gmail.com>
2021-09-30 18:43:45 -0300

    feat:configuraçao para que total nas compras fica com duas casas decimais

--- carrinho/src/components/Carrinho/index.jsx
+++ carrinho/src/components/Carrinho/index.jsx
@@ -3,8 +3,8 @@ import "./style.css"
 function Carrinho({carrinho}){
     return (
         <div className="displaycarrinho">
-            <p className="descri">Total: R$ {carrinho.reduce((a,e)=>a+Number(e.price),0)}</p>
-            <p className="descri">Desconto: R$ {carrinho.reduce((a,e)=>a+Number(e.discount),0)}</p>
+            <p className="descri">Total: R$ {Math.round(carrinho.reduce((a,e)=>a+Number(e.price),0)*100)/100}</p>
+            <p className="descri">Desconto: R$ {Math.round(carrinho.reduce((a,e)=>a+Number(e.discount),0)*100)/100}</p>
             <p className="descri">Carrinho:</p>
             <ul className="carrinho">
                 {console.log("carrinho: ",carrinho)}
@@ -12,8 +12,8 @@ function Carrinho({carrinho}){
                 <li key={i} className="itenscarrinho">
                     <p className="codigo">{e.code}</p>
                     <h4>{e.name}</h4>
-                    <p className="price">{e.price}</p>
-                    <p className="discount">{e.discount}</p>
+                    <p className="price">{Math.round(e.price*100)/100}</p>
+                    <p className="discount">{Math.round(e.discount*100)/100}</p>
                 </li>)}
             </ul>
         </div>
